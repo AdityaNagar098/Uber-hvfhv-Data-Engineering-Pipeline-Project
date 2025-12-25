234dfa8535dd3e21cbffd1071cacfdaf93eca817
Aditya Nagar <66547287+AdityaNagar098@users.noreply.github.com>
2025-12-25 23:03:07 +0530

    Enhance create_raw_tables.sql with task comments
    Added comments for database and schema creation tasks.

--- SQL/RAW/create_raw_tables.sql
+++ SQL/RAW/create_raw_tables.sql
@@ -1,2 +1,24 @@
 -- RAW layer table creation (schema inferred from Parquet)
 -- Data loaded from NYC TLC HVFHV Parquet files into Snowflake RAW schema
+
+-- task:- Create Database for the Project
+--sql code:-
+-- CREATE DATABASE UBER_ANALYTICS;
+
+-- task:- Create Schema (Organize Tables)
+
+-- sqlcode:-
+-- USE DATABASE UBER_ANALYTICS;
+-- CREATE SCHEMA RAW;
+-- CREATE SCHEMA SILVER;
+-- CREATE SCHEMA GOLD;
+
+-- task:-Verify Database & Schemas (Quick Check)
+
+-- sql code:- 
+SHOW DATABASES;
+
+USE DATABASE UBER_ANALYTICS;
+SHOW SCHEMAS;
+
+
